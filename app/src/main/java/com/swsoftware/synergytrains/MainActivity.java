37c02d4f3219a68b1298985c866ebd712b5d2fd0
saveliy <born120706@gmail.com>
2023-06-06 12:08:05 +0700

    done!

--- app/src/main/java/com/swsoftware/synergytrains/MainActivity.java
+++ app/src/main/java/com/swsoftware/synergytrains/MainActivity.java
@@ -1,48 +1,106 @@
 package com.swsoftware.synergytrains;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.Menu;
+import android.view.MenuItem;
 
-import java.util.Arrays;
-import java.util.Random;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.appcompat.content.res.AppCompatResources;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.FragmentManager;
+
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    ArrayList<Ticket> tickets = new ArrayList<>();
+    Toolbar toolbar;
+    MenuItem themeItem;
+    CollapsingToolbarLayout collapsingToolbarLayout;
+    TabLayout tabLayout;
 
-    @Override
+    FloatingActionButton buyFab;
+
     @SuppressLint("MissingInflatedId")
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView textView = findViewById(R.id.textView);
+        tickets.add(new Ticket(1, "asd", "qwe", "12.06.2023 12:00", null, 3000));
 
-        int[][] numbers = new int[10][10];
-        Random random = new Random();
+        toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("");
+        setSupportActionBar(toolbar);
+        collapsingToolbarLayout = findViewById(R.id.collapsingToolbar);
+        tabLayout = findViewById(R.id.tabLayout);
+        buyFab = findViewById(R.id.buyFab);
 
-        for (int x = 0; x < 10; x++) {
-            for (int y = 0; y < 10; y++) {
-                numbers[x][y] = random.nextInt(250);
-            }
-        }
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.frameLayout, new MyTicketsFragment())
+                .setReorderingAllowed(true)
+                .addToBackStack(null)
+                .commit();
 
-        for (int x = 0; x < 10; x++) {
-            int[] line = numbers[x];
-            numbers[x] = Arrays.stream(line).filter(
-                    (number) -> number % 2 != 0 & number <= 100
-            ).toArray();
-        }
-
-        StringBuilder stringBuilder = new StringBuilder();
-        for (int[] line : numbers) {
-            for (int number : line) {
-                stringBuilder.append(number).append(" ");
-            }
-            stringBuilder.append("\n");
-        }
-
-        textView.setText(stringBuilder);
     }
+
+    public void addTicket(Ticket ticket) {
+        tickets.add(ticket);
+        getSupportFragmentManager().popBackStack();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (getSupportFragmentManager().getBackStackEntryCount() > 0) {
+            getSupportFragmentManager().popBackStack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    //    @Override
+//    protected void onNightModeChanged(int mode) {
+//        super.onNightModeChanged(mode);
+//        if (mode == AppCompatDelegate.MODE_NIGHT_NO) {
+//            themeItem.setIcon(R.drawable.baseline_dark_mode_24);
+//        } else {
+//            themeItem.setIcon(R.drawable.baseline_light_mode_24);
+//        }
+//    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        themeItem = menu.findItem(R.id.theme);
+        if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES) {
+            themeItem.setIcon(R.drawable.baseline_dark_mode_24);
+        } else {
+            themeItem.setIcon(R.drawable.baseline_light_mode_24);
+        }
+        return super.onCreateOptionsMenu(menu);
+    }
+
+//    @SuppressLint("NonConstantResourceId")
+//    @Override
+//    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+//        switch (item.getItemId()) {
+//            case R.id.theme:
+//                if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES) {
+//                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+//                } else {
+//                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+//                }
+//                break;
+//            case R.id.language:
+//                break;
+//        }
+//        return super.onOptionsItemSelected(item);
+//    }
 }
